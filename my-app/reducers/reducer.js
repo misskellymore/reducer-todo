a3421ab10bb77987896709bfea718433228c030d
Kelly Moreira <moreira.kd@outlook.com>
2019-09-10 14:35:56 -0400

    step 1

---
+++ my-app/reducers/reducer.js
@@ -0,0 +1,13 @@
+
+export const initialState  = {
+
+    todos: [
+
+        {   item: 'Learn about reducers',
+            completed: false,
+            id: 3892987589
+          }
+    ]
+
+}
+
---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, {useReducer} from 'react';
+import './App.css';
+import {initialState, reducer} from '../reducers/reducer.js';
+
+function App() {
+
+  return (
+    <div className="App">
+    testing
+    </div>
+  );
+}
+
+export default App;
